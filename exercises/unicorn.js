637130acd4506367e49a87e464a8670aec93da23
Ryan Canton <ryancanton999@gmail.com>
2023-03-18 13:56:20 -0600

    Feat: Made tests for unicorn pass

--- exercises/unicorn.js
+++ exercises/unicorn.js
@@ -1,7 +1,25 @@
 class Unicorn {
   // Think of a constructor as being similar to initialize in Ruby
   // Instead of setting attributes like @name = name in JavaScript we say this.name = name
-  constructor() {
+  constructor(name, color) {
+    this.name = name;
+
+    if (color == null) {
+      color = 'white'
+    }
+    this.color = color;
+  }
+
+  isWhite() {
+    if (this.color != 'white') {
+      return false;
+    }
+    return true;
+  }
+
+  says(textToSay){
+    var textToSayFormatted = `**;* ${textToSay} *;**`;
+    return textToSayFormatted;
   }
 }
 
